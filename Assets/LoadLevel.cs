853fc295082d11f177640700d1881f3e616ce9de
Matt Smith <marfu2@hotmail.co.uk>
2019-09-20 03:53:07 +0100

    Added music and splash screen

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Invoke("LoadFirstScene", 2);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
